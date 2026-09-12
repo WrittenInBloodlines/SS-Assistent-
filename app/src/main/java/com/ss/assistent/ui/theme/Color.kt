cb4b6683fb019fde1cf6236fcc34b5a03be61a1d
S.S. <safijasamsudinova@gmail.com>
2026-09-12 11:21:01 +0200

    Add SS Assistent color palette

---
+++ app/src/main/java/com/ss/assistent/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.ss.assistent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Night = Color(0xFF09070F)
+val DeepPurple = Color(0xFF171126)
+val Purple = Color(0xFF9B7CFF)
+val SoftPurple = Color(0xFFC5B5FF)
+val TextPrimary = Color(0xFFF5F1FF)
+val TextSecondary = Color(0xFFAAA3B8)
